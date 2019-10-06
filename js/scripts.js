3d856f4f7b030495ca17695f936a6567e3f8c52c
Peter Kamau <peterndungukamau9@gmail.com>
2019-10-06 17:24:52 +0300

    change pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ function PlaceOrder (size ,crust ,toppings){
 }
 
 var sizePizza = ["Small","Medium","Large"];
-var toppingsPizza = ["topping1","topping2","topping3"];
+var toppingsPizza = ["Pepperoni","Extra cheese","Green peppers"];
 var crustPizza = ["Thin Crust","Thick Crust","Cheese Filled Crust","Stuffed Crust"];
 
 PlaceOrder.prototype.cost = function(){
 if(this.size === sizePizza[0]){
-    this.price += 200;
+    this.price += 100;
     }
 
 else if(this.size === sizePizza[1]){
@@ -22,7 +22,7 @@ else if(this.size === sizePizza[1]){
     }
 
 else if(this.size === sizePizza[2]){
-    this.price += 100;
+    this.price += 200;
  }
 
  if(this.crust === crustPizza[0]){
@@ -60,23 +60,6 @@ return this.price
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // user interface logic
 $(document).ready(function(){
     $("button#submit").click(function(event) {
@@ -89,7 +72,7 @@ $(document).ready(function(){
         newPizzaOrder.cost();
         totalCosts.push(newPizzaOrder.price);
     
-        alert(size + crust + toppings + name + totalCosts);
+        alert("Your Pizza order is " + name + " " + size + " "  + toppings + " " + crust + " and the total cost is " + totalCosts + " .Thanks for ordering we will deliver shortly.");
     });
 
     
